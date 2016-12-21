27d1426933b044b09192cd137089083e0d02e4d5
bjornakr <bjornakr@gmail.com>
2016-12-21 13:15:23 +0100

    Refactored mapper, removed validation. Validation happens in domain now.

--- src/main/scala/application/CardRequestMapper.scala
+++ src/main/scala/application/CardRequestMapper.scala
@@ -4,21 +4,9 @@ import domain.{Back, Card, Front}
 import infrastructure.{SystemMessage, SystemMessages}
 
 object CardRequestMapper {
-    private def checkEmpty(request: Dto.CreateCardRequest): Either[SystemMessage, Dto.CreateCardRequest] = {
-        def cannotBeEmpty(field: String) = Left(SystemMessages.CannotBeEmpty(field))
-
-        request match {
-            case Dto.CreateCardRequest(None, _, _) => cannotBeEmpty("front")
-            case Dto.CreateCardRequest(_, None, _) => cannotBeEmpty("back")
-            case Dto.CreateCardRequest(_, _, None) => cannotBeEmpty("exampleOfUse")
-            case _ => Right(request)
-        }
-    }
-
     def apply(request: Dto.CreateCardRequest): Either[SystemMessage, Card] =
         for {
-            req <- checkEmpty(request).right
-            front <- Front(req.front.getOrElse("")).right
-            back <- Back(req.back.getOrElse(""), req.exampleOfUse.getOrElse("")).right
+            front <- Front(request.front.getOrElse("")).right
+            back <- Back(request.back.getOrElse(""), request.exampleOfUse.getOrElse("")).right
         } yield Card(front, back)
 }
